b7d17a3fca046882f28b0f5a528711a6d85d2495
sidaliam <sidaliamo18@gmail.com>
2024-05-11 15:13:19 +0100

    KIRAA V3

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'adminkiraa',
+  webDir: 'build'
+};
+
+export default config;
